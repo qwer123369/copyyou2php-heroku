6fd891c96142d5738e9e36e51e8a48f37320ec09
qwer123369 <43739113+qwer123369@users.noreply.github.com>
2020-11-01 20:14:32 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');//这里改成您的api key
-define('GJ_CODE', 'NZ');//国家代码
-define('SITE_NAME', 'Share2018');//网站名字
-define('TITLENAME', '共享網飛群&遊管');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'wingwanwin@gmail.com');//邮箱
+define('APIKEY', 'AIzaSyAN_vy03_ddfKCITGuLOHBW3vk32YND36c');//这里改成您的api key
+define('GJ_CODE', 'HK');//国家代码
+define('SITE_NAME', '潘多拉');//网站名字
+define('TITLENAME', '潘多拉');//网站名字
+define('EN2DEKEY', 'AnQUP1JFGG435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'boyijiayuan@gmail.com');//邮箱
 ?>
